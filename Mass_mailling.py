18ac49566887867606b57503a63d3ae17dd7663e
E-Nico <nicomagny@outlook.fr>
2024-10-11 19:58:20 +0200

    Update python

--- Mass_mailling.py
+++ Mass_mailling.py
@@ -10,16 +10,19 @@
 import csv
 import os
 import argparse
+import pandas as pd
 
 
 # Création d'un parser avec une description plus détaillée
-parser = argparse.ArgumentParser(description='Description of your program')
+parser = argparse.ArgumentParser(description="Ce soft permet d'envoyer des mails personnalisés en masse. Pensez à bien formater votre CSV afin de ne pas avoir de problème.\nLe nom des colonnes du CSV doit être identiques aux variables dans le mail")
+
 
 # Définition des arguments sans répétition des valeurs
-parser.add_argument('-id', '--identifiant', help="Identifiant unique de l'application",metavar='', required=True)
-parser.add_argument('-a', '--appname', help="Nom de l'application",metavar='', required=True)
-parser.add_argument('-ao', '--appofficier', help="Prénom de l'application officier",metavar='', required=True)
-parser.add_argument('-m', '--mail', help="mail de l'application officier",metavar='', required=True)
+parser.add_argument('-c', '--csv', help="fichier csv avec l'ensemble des informations nécessaires",metavar='', required=True)
+#parser.add_argument('-id', '--identifiant', help="identifiant unique de l'application",metavar='', required=True)
+#parser.add_argument('-a', '--appname', help="nom de l'application",metavar='', required=True)
+#parser.add_argument('-ao', '--appofficier', help="prénom de l'application officier",metavar='', required=True)
+#parser.add_argument('-m', '--mail', help="mail de l'application officier",metavar='', required=True)
 
 
 
@@ -27,15 +30,15 @@ parser.add_argument('-m', '--mail', help="mail de l'application officier",metava
 args = parser.parse_args()
 
 def get_data_csv():
-    print("")
-    with open('eggs.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            print(', '.join(row))
+    df = pd.read_csv(args.csv)
+    header = df.head(0)
+    dictionnaire = df.to_dict()
+    for head in header:
+        print(dictionnaire[head][0])
 
 
 def get_mail_template():
     print("Ok ça fonctionne !")
 
 if __name__ == "__main__" :
-    get_mail_template()
+    get_data_csv()
